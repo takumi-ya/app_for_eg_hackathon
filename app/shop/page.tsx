97a18fe8886fd15126373b2d7f78571c80b88c2e
kurumayusamugariko <junmomo6780@gmail.com>
2024-11-08 19:42:32 +0000

    shop

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -14,10 +14,11 @@ export default function HomePage() {
   }
 
   return (
-    <div className="menu-container">
-      <div className="button-container">
+    <div className="menu-container h-full flex flex-col justify-center items-center" >
+      <div className="button-container flex flex-wrap justify-center">
         <button className="button" onClick={goToMenuList}>メニュー一覧</button>
         <button className="button" onClick={goToShopInfo}>店舗情報</button>
+        <button className="button" onClick={goToShopInfo}>予約確認</button>
         
       </div>
     </div>
